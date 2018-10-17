0e8a214e1984af685798ab00da68b639a9bd82b9
Adrian <adrian.b.p03@gmail.com>
2018-10-17 16:52:19 -0600

    Fixing things

--- components/tratamiento/models.py
+++ components/tratamiento/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from components.experimento.models import Experimento
 
 class Tratamiento(models.Model):
+    experimento = models.ForeignKey(Experimento, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=100)
     fch_inicio = models.DateTimeField()
     fch_fin = models.DateTimeField()
     objects = models.Manager()
+
 
 class ObservacionTratamiento(models.Model):
     descripcion = models.CharField(max_length=200)
